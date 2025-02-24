bc5e8d4f106c067aa6b17c7b8072694624f419fc
gabriel <gabriel@digitalk.com.br>
2025-02-24 16:37:42 -0300

    added roll_dice method

--- diceroller.py
+++ diceroller.py
@@ -1,64 +1,69 @@
 from operator import mod
 import random, sys
 
+def roll_dice(diceStr):
+    diceStr = diceStr.lower().replace(' ', '')
+    # Find the "d" in the dice string input:
+    dIndex = diceStr.find('d')
+    if dIndex == -1:
+        raise Exception('Missing the "d" character.')
+
+    # Find number of dice
+    numberOfDice = diceStr[:dIndex]
+    if not numberOfDice.isdecimal():
+        raise Exception('Missing the number of dice.')
+    numberOfDice = int(numberOfDice)
+
+    # Find Modifier
+    modIndex = diceStr.find('+')
+    if modIndex == -1:
+        modIndex = diceStr.find('-')
+    
+    # Find number of sides
+    if modIndex == -1:
+        numberOfSides = diceStr[dIndex + 1 :]
+    else:
+        numberOfSides = diceStr[dIndex + 1 : modIndex]
+    if not numberOfSides.isdecimal():
+        raise Exception('Missing number of sides.')
+    numberOfSides = int(numberOfSides)
+
+    # Modifier
+    if modIndex == -1:
+        mod = 0
+    else:
+        mod = int(diceStr[modIndex + 1 :])
+        if diceStr[modIndex] == '-':
+            mod = -mod
+
+    # Roll dice
+    rolls = []
+    for i in range(numberOfDice):
+        roll = random.randint(1, numberOfSides)
+        rolls.append(roll)
+
+    return rolls, mod
+
 def __main__():
     while True:
         try:
             diceStr = input('> ')  # The prompt to enter the dice string.
-            if diceStr.upper() == 'QUIT':
+            if (diceStr.upper() == 'QUIT')or(diceStr.upper() == 'EXIT'):
                 print('Thanks for playing!')
                 sys.exit()
 
-            diceStr = diceStr.lower().replace(' ', '')
-            # Find the "d" in the dice string input:
-            dIndex = diceStr.find('d')
-            if dIndex == -1:
-                raise Exception('Missing the "d" character.')
-
-            # Find number of dice
-            numberOfDice = diceStr[:dIndex]
-            if not numberOfDice.isdecimal():
-                raise Exception('Missing the number of dice.')
-            numberOfDice = int(numberOfDice)
-
-            # Find Modifier
-            modIndex = diceStr.find('+')
-            if modIndex == -1:
-                modIndex = diceStr.find('-')
-            
-            # Find number of sides
-            if modIndex == -1:
-                numberOfSides = diceStr[dIndex + 1 :]
-            else:
-                numberOfSides = diceStr[dIndex + 1 : modIndex]
-            if not numberOfSides.isdecimal():
-                raise Exception('Missing number of sides.')
-            numberOfSides = int(numberOfSides)
-
-            # Modifier
-            if modIndex == -1:
-                modAmount = 0
-            else:
-                modAmount = int(diceStr[modIndex + 1 :])
-                if diceStr[modIndex] == '-':
-                    modAmount = -modAmount
-
-            # Roll dice
-            rolls = []
-            for i in range(numberOfDice):
-                roll = random.randint(1, numberOfSides)
-                rolls.append(roll)
+            rolls, mod = roll_dice(diceStr)
 
             # Total
-            print('Total:', sum(rolls) + modAmount, '( ', end='')
+            print('Total:', sum(rolls) + mod, '( ', end='')
 
             for i, roll in enumerate(rolls):
                 rolls[i] = str(roll)
             print(', '.join(rolls), end='')
 
-            if modAmount != 0:
-                modSign = diceStr[modIndex]
-                print(' ) {} {}'.format(modSign, abs(modAmount)))
+            if mod != 0:
+                modSign = '+' if mod > 0 else '-'
+                print(' ) {} {}'.format(modSign, abs(mod)))
             else :
                 print(' )')
 
