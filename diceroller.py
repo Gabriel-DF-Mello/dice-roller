13a18e3f5ad6d53856379e7b745513455ecd8887
gabriel <gabriel@digitalk.com.br>
2025-02-25 16:57:21 -0300

    added file system

--- diceroller.py
+++ diceroller.py
@@ -1,7 +1,7 @@
-from operator import mod
 import random, sys
 
 memory = {}
+dicebag = 'dicebag.txt'
 
 def roll_dice(diceStr):
     diceStr = diceStr.lower().replace(' ', '')
@@ -32,11 +32,11 @@ def roll_dice(diceStr):
 
     # Modifier
     if modIndex == -1:
-        mod = 0
+        modifier = 0
     else:
-        mod = int(diceStr[modIndex + 1 :])
+        modifier = int(diceStr[modIndex + 1 :])
         if diceStr[modIndex] == '-':
-            mod = -mod
+            modifier = -modifier
 
     # Roll dice
     rolls = []
@@ -44,7 +44,7 @@ def roll_dice(diceStr):
         roll = random.randint(1, numberOfSides)
         rolls.append(roll)
 
-    return rolls, mod
+    return rolls, modifier
 
 def save_roll(name, diceStr):
     global memory
@@ -59,23 +59,58 @@ def remove_saved(name):
     memory.pop(name)
 
 def save_file(path):
+    global memory
+    with open(path, "w") as f:
+        for key, val in memory.items():
+            f.write(f"{key}:{val}\n")
     return
 
 def load_file(path):
+    global memory
+    with open(path, 'r') as file:
+        for line in file:
+            s = line.strip().split(':')
+            memory[s[0]] = s[1]
     return
 
 def __main__():
+    global memory
+    global dicebag
+    
+    load_file(dicebag)
+    print('Type the commands (info) or (help) for instructions')
     while True:
         try:
             dice_input = input('> ')  # The prompt to enter the dice string.
             if (dice_input.upper() == 'QUIT')or(dice_input.upper() == 'EXIT')or(dice_input.upper() == 'Q'):
                 print('Thanks for playing!')
+                save_file(dicebag)
                 sys.exit()
 
-            if (dice_input.upper()[:4] == 'INFO'):
+            if (dice_input.strip() == ''):
                 continue
 
-            if (dice_input.upper()[:8] == 'KEEP'):
+            if (dice_input.upper()[:4] == 'READ') or (dice_input.upper()[:8] == 'REMEMBER'):
+                print(memory)
+                continue
+
+            if (dice_input.upper()[:4] == 'INFO') or (dice_input.upper()[:4] == 'HELP'):
+                print('Here is a list of commands:')
+                print('\troll (xdy): rolls x many dice, each with y sides, for example: roll 4d6+3')
+                print('\twrite (name of formula) (xdy): saves a certain formula under the given name, to be easily rolled later, for example: write fireball 8d6')
+                print('\tuse (name of formula): rolls a previously saved formula, for example: use fireball')
+                print('\tforget (name of formula): deletes a formula from memory, for example: forget fireball')
+                print('\tread: view your formulas currently in memory')
+                print('\tsave: saves your current memory to a file for later use')
+                print('\tquit: exits the game and saves your memory')
+                continue
+
+            if (dice_input.upper()[:4] == 'SAVE'):
+                save_file(dicebag)
+                print('Memory saved!')
+                continue
+
+            if (dice_input.upper()[:5] == 'WRITE'):
                 sinput = dice_input.split(' ')
                 name = ' '.join(sinput[1:-1])
                 save_roll(name, sinput[-1])
@@ -92,23 +127,23 @@ def __main__():
             if (dice_input.upper()[:3] == 'USE'):
                 sinput = dice_input.split(' ')
                 name = ' '.join(sinput[1:])
-                rolls, mod = roll_saved(name)
+                rolls, modifier = roll_saved(name)
 
             if (dice_input.upper()[:4] == 'ROLL'):
                 sinput = dice_input.split(' ')
                 dice = ' '.join(sinput[1:])
-                rolls, mod = roll_dice(dice)
+                rolls, modifier = roll_dice(dice)
             
             # Total
-            print('Total:', sum(rolls) + mod, '( ', end='')
+            print('Total:', sum(rolls) + modifier, '( ', end='')
 
             for i, roll in enumerate(rolls):
                 rolls[i] = str(roll)
             print(', '.join(rolls), end='')
 
-            if mod != 0:
-                modSign = '+' if mod > 0 else '-'
-                print(' ) {} {}'.format(modSign, abs(mod)))
+            if modifier != 0:
+                modSign = '+' if modifier > 0 else '-'
+                print(' ) {} {}'.format(modSign, abs(modifier)))
             else :
                 print(' )')
 
